d6335989f338e8298bbb432b71271be23e26320b
0xPavelTkachenko <paveltkachenko@email.com>
2018-10-09 13:43:53 +0300

    init commit

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'production',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'index.js'
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      filename: 'index.html',
+      template: './src/index.pug'
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.pug$/,
+        exclude: /(node_modules|.git)/,
+        loader: 'pug-loader'
+      },
+      {
+        test: /\.styl$/,
+        exclude: /(node_modules|.git)/,
+        use: ['style-loader', 'css-loader', 'stylus-loader']
+      }
+    ]
+  }
+}
