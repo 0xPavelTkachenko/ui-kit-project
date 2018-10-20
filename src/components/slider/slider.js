a70c640ba7fce6e368b947199d6ec2d8fd45d90d
0xPavelTkachenko <paveltkachenko@email.com>
2018-10-21 01:38:48 +0300

    add slider component

---
+++ src/components/slider/slider.js
@@ -0,0 +1,13 @@
+'use strict';
+
+import 'ion-rangeslider';
+import 'ion-rangeslider/css/ion.rangeSlider.css';
+import 'ion-rangeslider/css/ion.rangeSlider.skinHTML5.css';
+
+const $ = require('jquery');
+
+document.querySelectorAll('input.slider').forEach((slider) => {
+  $(slider).ionRangeSlider({keyboard: false});
+  $(slider).data('ionRangeSlider').update();
+  slider.remove();
+});
