756782eb221350a2176b7316cf5b3896a62ae198
0xPavelTkachenko <paveltkachenko@email.com>
2019-03-14 02:24:25 +0700

    refactor percentage-circle component

--- src/components/percentage-circle/percentage-circle.js
+++ src/components/percentage-circle/percentage-circle.js
@@ -1,14 +1,40 @@
-'use strict';
+import $ from 'jquery';
 
-import './circliful/js/jquery.circliful.min.js';
-import './circliful/css/jquery.circliful.css';
+import '../../../libs/percentage-circle/circliful/js/jquery.circliful.min';
+import '../../../libs/percentage-circle/circliful/css/jquery.circliful.css';
 
-document.querySelectorAll('.percentage-circle').forEach((percentageCircle) => {
-  $(percentageCircle).circliful({
-    backgroundColor: 'none',
-    foregroundBorderWidth: 6,
-    noPercentageSign: true,
-    percentageTextSize: '',
-    fontColor: '#9D9D9D'
-  });
-});
+class PercentageCircle {
+  constructor(root, id) {
+    this._$root = $(root);
+    this._id = id;
+    this._useJqueryPlugin();
+  }
+
+  toString() {
+    return `{"class": "PercentageCircle", "id": "${this._id}"}`;
+  }
+
+  _useJqueryPlugin() {
+    const $percentageCircle = this._$root;
+
+    $percentageCircle.circliful(
+      {
+        backgroundColor: 'none',
+        foregroundBorderWidth: 6,
+        noPercentageSign: true,
+        percentageTextSize: '',
+        fontColor: '#9D9D9D',
+      },
+    );
+  }
+}
+
+const createPercentage = function createPercentageCircle(id) {
+  new PercentageCircle(this, id);
+};
+
+const $percentageCircles = $('.js-percentage-circle');
+
+$percentageCircles.each(createPercentage);
+
+export default PercentageCircle;
