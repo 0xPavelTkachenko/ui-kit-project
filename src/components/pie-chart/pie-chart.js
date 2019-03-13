489431650f8ca32b8444e52f73a2fac3c673042f
0xPavelTkachenko <paveltkachenko@email.com>
2019-03-14 00:30:18 +0700

    refactor pie-chart component

--- src/components/pie-chart/pie-chart.js
+++ src/components/pie-chart/pie-chart.js
@@ -1,14 +1,40 @@
-'use strict';
+import $ from 'jquery';
 
-import './donut/jquery.donut.js';
+import '../../../libs/pie-chart/donut/js/jquery.donut';
 
-document.querySelectorAll('.pie-chart').forEach((pieChart) => {
-  let colors = [];
-  pieChart.querySelectorAll('.pie-chart__data').forEach((data) => {
-    colors.push(data.getAttribute('data-color') || '#000');
-  });
+class PieChart {
+  constructor(root, id) {
+    this._$root = $(root);
+    this._id = id;
+    this._useJqueryPlugin();
+  }
 
-  $(pieChart).donut({
-    colors: colors
-  });
-});
+  toString() {
+    return `{"class": "PieChart", "id": "${this._id}"}`;
+  }
+
+  _useJqueryPlugin() {
+    const $pieChart = this._$root;
+    const colors = [];
+
+    $pieChart.find('.pie-chart__data').each(function getColors() {
+      colors.push($(this).attr('data-color') || '#000');
+    });
+
+    $pieChart.donut(
+      {
+        colors,
+      },
+    );
+  }
+}
+
+const createPc = function createPieChart(id) {
+  new PieChart(this, id);
+};
+
+const $pieCharts = $('.js-pie-chart');
+
+$pieCharts.each(createPc);
+
+export default PieChart;
