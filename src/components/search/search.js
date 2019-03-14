0ea155ec41728c7c5c19ff921ae1c81d84a6a37e
0xPavelTkachenko <paveltkachenko@email.com>
2019-03-14 16:48:38 +0700

    refactor search component

--- src/components/search/search.js
+++ src/components/search/search.js
@@ -1,15 +1,35 @@
-'use strict';
+import $ from 'jquery';
 
-document.querySelectorAll('.search').forEach((search) => {
-  let input = search.querySelector('.search__input');
-  let placeholder = input.getAttribute('placeholder') || '';
-  let notFoundMsg = input.getAttribute('data-not-found-msg') || '';
+class Search {
+  constructor(root, id) {
+    this._$root = $(root);
+    this._id = id;
 
-  setInterval(() => {
-    if ($(search).hasClass('search_not-found')) {
-      input.setAttribute('placeholder', notFoundMsg);
+    setInterval(this._watchFailedState.bind(this), 100);
+  }
+
+  toString() {
+    return `{"class": "Search", "id": "${this._id}"}`;
+  }
+
+  _watchFailedState() {
+    const $search = this._$root;
+    const $input = $search.find('.search__input');
+    const placeholder = $input.attr('data-placeholder') || '';
+    const notFoundMessage = $input.attr('data-not-found-message') || '';
+
+    if ($search.hasClass('search_failed')) {
+      $input.attr('placeholder', notFoundMessage);
     } else {
-      input.setAttribute('placeholder', placeholder);
+      $input.attr('placeholder', placeholder);
     }
-  }, 100);
+  }
+}
+
+const $searches = $('.js-search');
+
+$searches.each(function createSearch(id) {
+  new Search(this, id);
 });
+
+export default Search;
