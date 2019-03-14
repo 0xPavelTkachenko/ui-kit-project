3e286809e6ff79b4460ed09e09bcccb71986c4ba
0xPavelTkachenko <paveltkachenko@email.com>
2019-03-14 22:29:59 +0700

    refactor calendar component

--- src/components/calendar/calendar.js
+++ src/components/calendar/calendar.js
@@ -1,23 +1,50 @@
-'use strict';
+import $ from 'jquery';
 
-import './air-datepicker/dist/js/datepicker.min.js';
-import './air-datepicker/dist/js/i18n/datepicker.en.js';
-import './air-datepicker/dist/css/datepicker.min.css';
+import '../../../libs/calendar/air-datepicker/css/datepicker.min.css';
+import '../../../libs/calendar/air-datepicker/js/datepicker.min';
+import '../../../libs/calendar/air-datepicker/js/i18n/datepicker.en';
 
-$('.calendar').each(function() {
-  $(this).datepicker({
-    language: 'en',
-    firstDay: 1,
-    todayButton: new Date(),
-    toggleSelected: false,
-    keyboardNav: false,
-    navTitles: {
-      days: 'MM'
-    },
-    onSelect: function(formattedDate, date, inst) {
-      inst.$el.find('.datepicker').attr('data-date', date.getDate());
-    }
-  });
+class Calendar {
+  constructor(root, id) {
+    this._$root = $(root);
+    this._id = id;
+    this._useJqueryPlugin();
+    this._selectCurrentDate();
+  }
 
-  $(this).find('.datepicker--button[data-action="today"]').trigger('click');
+  toString() {
+    return `{"class": "Calendar", "id": "${this._id}"}`;
+  }
+
+  _useJqueryPlugin() {
+    const $calendar = this._$root;
+
+    $calendar.datepicker(
+      {
+        language: 'en',
+        firstDay: 1,
+        todayButton: new Date(),
+        toggleSelected: false,
+        keyboardNav: false,
+        navTitles: {
+          days: 'MM',
+        },
+        onSelect(formattedDate, date) {
+          $calendar.find('.datepicker').attr('data-date', date.getDate());
+        },
+      },
+    );
+  }
+
+  _selectCurrentDate() {
+    this._$root.datepicker().data('datepicker').selectDate(new Date());
+  }
+}
+
+const $calendars = $('.js-calendar');
+
+$calendars.each(function createCalendar(id) {
+  new Calendar(this, id);
 });
+
+export default Calendar;
