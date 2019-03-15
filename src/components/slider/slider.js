0da251fddcb6b8acbe541cc3fac1a1e4dc97a5c5
0xPavelTkachenko <paveltkachenko@email.com>
2019-03-15 15:21:49 +0700

    refactor slider component

--- src/components/slider/slider.js
+++ src/components/slider/slider.js
@@ -1,11 +1,36 @@
-'use strict';
-
+import $ from 'jquery';
 import 'ion-rangeslider';
 import 'ion-rangeslider/css/ion.rangeSlider.css';
 import 'ion-rangeslider/css/ion.rangeSlider.skinHTML5.css';
 
-document.querySelectorAll('input.slider').forEach((slider) => {
-  $(slider).ionRangeSlider({keyboard: false});
-  $(slider).data('ionRangeSlider').update();
-  slider.remove();
+class Slider {
+  constructor(root, id) {
+    this._$root = $(root);
+    this._id = id;
+    this._useJqueryPlugin();
+  }
+
+  toString() {
+    return `{"class": "Slider", "id": "${this._id}"}`;
+  }
+
+  _useJqueryPlugin() {
+    const $input = this._$root.find('input');
+
+    $input.ionRangeSlider({ keyboard: false });
+
+    $('document').ready(this._recalculateSlider.bind($input));
+  }
+
+  _recalculateSlider() {
+    this.data('ionRangeSlider').update();
+  }
+}
+
+const $sliders = $('.js-slider');
+
+$sliders.each(function createSlider(id) {
+  new Slider(this, id);
 });
+
+export default Slider;
