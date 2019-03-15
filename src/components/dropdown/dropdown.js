1916faf574e0a09d32f0515d874a5f016e297e5c
0xPavelTkachenko <paveltkachenko@email.com>
2019-03-16 01:45:32 +0700

    refactor dropdown component

--- src/components/dropdown/dropdown.js
+++ src/components/dropdown/dropdown.js
@@ -1,14 +1,35 @@
-'use strict';
+import $ from 'jquery';
 
-import './minimalect/jquery.minimalect.min.js';
-import './minimalect/jquery.minimalect.min.css';
+import '../../../libs/dropdown/minimalect/js/jquery.minimalect.min';
+import '../../../libs/dropdown/minimalect/css/jquery.minimalect.min.css';
 
-document.querySelectorAll('select.dropdown').forEach((dropdown) => {
-  $(dropdown).minimalect({
-    placeholder: dropdown.getAttribute('data-placeholder'),
-    empty: dropdown.getAttribute('data-emptyMsg'),
-    searchable: false
-  });
-  $(dropdown).next().addClass(dropdown.classList.value);
-  dropdown.remove();
+class Dropdown {
+  constructor(root, id) {
+    this._$root = $(root);
+    this._id = id;
+    this._useJqueryPlugin();
+  }
+
+  toString() {
+    return `{"class": "Dropdown", "id": "${this._id}"}`;
+  }
+
+  _useJqueryPlugin() {
+    const $select = this._$root.find('select');
+
+    $select.minimalect(
+      {
+        placeholder: this._$root.attr('data-placeholder'),
+        searchable: false,
+      },
+    );
+  }
+}
+
+const $dropdowns = $('.js-dropdown');
+
+$dropdowns.each(function createDropdown(id) {
+  new Dropdown(this, id);
 });
+
+export default Dropdown;
