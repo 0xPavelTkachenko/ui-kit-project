8e0c8ed7bb31f4eb27a6dee4b83bab7b3167b1d4
0xPavelTkachenko <paveltkachenko@email.com>
2019-04-04 17:58:30 +0700

    fix map.js

--- src/components/map/map.js
+++ src/components/map/map.js
@@ -13,7 +13,7 @@ class Map {
   }
 
   _useYandexMaps() {
-    ymaps.load().then(maps => {
+    ymaps.load().then((maps) => {
       const $map = this._$root;
       const mapWindow = $map.find('.map__window').get(0);
       const mapOptions = {
@@ -28,14 +28,14 @@ class Map {
         const markerCoords = [+$map.attr('data-marker-lat') || 0, +$map.attr('data-marker-lng') || 0];
         const markerOptions = {
           iconLayout: 'default#image',
-          iconImageHref: './assets/marker.png',
+          iconImageHref: './assets/images/marker.png',
           iconImageSize: [59, 60],
           iconImageOffset: [-20, -54],
         };
         const marker = new maps.Placemark(markerCoords, {}, markerOptions);
         yandexMap.geoObjects.add(marker);
       }
-    }).catch(error => console.log('FAIL: ', error));
+    });
   }
 }
 
